3d37eaca8b8417f15187289375bb6a2b2616b746
Adrian <adrian.nenu@gmail.com>
2018-02-03 13:02:08 +0000

    remove chat

--- game/layout/templates/organization/organization_view.tpl
+++ game/layout/templates/organization/organization_view.tpl
@@ -6,13 +6,13 @@
             <img src="{$config.url}layout/img/characters/Irene-A.I.png" style=" max-width:240px" title="Welcome to {$org.name}"/>
           </a>
             </div>
-          
+
         </div>
         <div class="col-md-9">
           <div class="text-center">
             <h1><a href="{$config.url}organization/show/{$org.id}/">{$org.name}</a></h1>
 			<h4>alias [{$org.tag}]{if $org.id eq $user.organization}, ran by {$org.ownern|profile_link}{/if}</h3>
-            <p>	
+            <p>
               {if $org.orank > 0}
                 Ranked #{$org.orank} in the world with {$org.points|number_format} points{else}not ranked{/if} - {$org.nrm}/{$org.max_nrm|number_format} members - {$org.wars_won|number_format} wars won out of {$org.wars} fought</p>
             {if $org.myOrg}
@@ -20,36 +20,33 @@
               <p>Your rank is {$access.name}</p>
             {/if}
           </div><br/>
- 
-           
-            
-            
-            <div class="futureNav middle"> 
-              <ul> 
+
+
+
+
+            <div class="futureNav middle">
+              <ul>
               {if $access}
-                <!--
-                  <li><a href="{$config.url}organization/show/{$org.id}/"><span class="glyphicon glyphicon-eye-open"></span></a></li>-->
-                   <li><a href="{$config.url}organization/show/{$org.id}/view/chat"><span class="glyphicon glyphicon-bullhorn"></span></a></li>		
-                 <li><a href="{$config.url}organization/show/{$org.id}/view/forum" title="Our private Forum"><span class="glyphicon glyphicon-comment"></span></a></li>		
-                
+                 <li><a href="{$config.url}organization/show/{$org.id}/view/forum" title="Our private Forum"><span class="glyphicon glyphicon-comment"></span></a></li>
+
                    <li>
                     <a href="{$config.url}organization/show/{$org.id}/view/wars">
                       {if $org.wars_inprogress}<strong>WAR IN TOW</strong>{else}<span class="glyphicon glyphicon-fire"></span>{/if}
                     </a>
-                  </li>		
-				  
+                  </li>
+
                  {if $access.viewMembers}
-                  <li><a href="{$config.url}organization/show/{$org.id}/view/members/"><span class="glyphicon glyphicon-user"></span></a></li>		
+                  <li><a href="{$config.url}organization/show/{$org.id}/view/members/"><span class="glyphicon glyphicon-user"></span></a></li>
                 {/if}
                 {if $access.manageApplications and $org.applications}
-                  <li><a href="{$config.url}organization/show/{$org.id}/view/applications">App's ({$org.applications})</a></li>		
+                  <li><a href="{$config.url}organization/show/{$org.id}/view/applications">App's ({$org.applications})</a></li>
                 {/if}
-          
-          
+
+
                 {if $access.manageOrganization}
-                  <li><a href="{$config.url}organization/show/{$org.id}/view/core"><span class="glyphicon glyphicon-cog"></span></a></li>		
+                  <li><a href="{$config.url}organization/show/{$org.id}/view/core"><span class="glyphicon glyphicon-cog"></span></a></li>
                 {/if}
-                {if !$access.owner} 
+                {if !$access.owner}
                   <li><a href="{$config.url}organization/show/{$org.id}/view/leave">Leave</a>	</li>
                 {/if}
               {else}
@@ -62,12 +59,12 @@
                     {elseif !$logged}
                       <li><a href="{$config.url}organization/show/{$org.id}/">Connect</a></li>
                     {/if}
-                    
+
                     <li><a href="{$config.url}organization/show/{$org.id}/view/wars">Wars</a></li>
-            
+
                 {/if}
-              </ul> 
-              
+              </ul>
+
             </div>
             {include file="error_success.tpl"}
             <div class="alert alert-info text-center">
@@ -76,14 +73,14 @@
             </strong>
             </div>
             {if !$access && !$org.allow_app}
-              
+
               <div class="alert alert-danger">
                   {$org.name} does not accept new members right now.
-              </div>     
-             
+              </div>
+
             {/if}
-               
-          
+
+
            {if !$load}
            {include file="organization/organization_main.tpl"}
            {elseif $load eq "forum"}
@@ -114,18 +111,15 @@
            {include file="organization/organization_hackingPoints.tpl"}
            {elseif $load eq "quit"}
            {include file="organization/organization_quit.tpl"}
-          
+
             {elseif $load eq "wars"}
-          
+
               {include file="organization/wars/organization_wars.tpl"}
-       
+
            {elseif $load eq "messageAll"}
-         
+
            {include file="organization/organization_mesall.tpl"}
-           {elseif $load eq "chat"}
-           {include file="chat/chat.tpl"}
         
            {/if}
       </div>
     </div>
-   
--- game/modules/organization/org_my_org.php
+++ game/modules/organization/org_my_org.php
@@ -2,56 +2,56 @@
 // can user manager wars?
     if ($oclass->perms['manage_wars']) {
       // check of any war requests have been sent to org
-      $warRequests = 
+      $warRequests =
       $db->where('status = 1')->wherE('org2=?', array($oclass->organization['id']))
          ->getOne('org_wars_reqs', 'count(id)');
 
       $oclass->organization['war_requests'] = $warRequests['requests'];
-      
+
     } // if wars
-    
-    
+
+
     if ($GET['view'] == 'members' && $oclass->perms['viewMembers']) {
-      
+
       $pages = new Paginator;
       $pages->items_total = $oclass->organization["nrm"];
-      $pages->mid_range = 5; 
+      $pages->mid_range = 5;
       $pages->items_per_page = 15;
       $pages->paginate();
-      
+
       $templateVariables['load']    = 'members';
       $members                      = $oclass->get_members($pages->limit);
       $templateVariables['members'] = $members;
-      
+
     } //$GET['view'] == 'members' && $oclass->perms['viewMembers']
     else if ($GET['view'] == 'messageAll' && $oclass->perms['messageAll']) {
       $templateVariables['load'] = 'messageAll';
-      
+
       $oclass->mes_all();
-      
+
       $ranks = $db->where('org_id', $oclass->organization['id'])->get('org_ranks', null, 'name, rank_id');
-      
+
       $members = $oclass->get_members($oclass->organization);
-      
+
       $templateVariables['ranks']   = $ranks;
       $templateVariables['members'] = $members;
     } //$GET['view'] == 'messageAll' && $oclass->perms['messageAll']
     else if ($GET['view'] == 'manageMembers' && $oclass->perms['manageMembers']) {
       $templateVariables['load'] = 'manageMembers';
-      
+
       if ($user_id = $_POST['kickMember']) {
         $member = $db->where('organization', $oclass->organization['id'])->where('id', $user_id)->join('org_ranks', 'rank_id = org_group', 'left outer')->where('owner_rank', 0)->getOne('users', 'username, id');
-       
+
         if ($_POST['do']) {
           $oclass->kickMember($member);
           $cardinal->redirect($url);
         } //$_POST['do']
-        
+
         $templateVariables['load']   = 'kick_member';
         $templateVariables['member'] = $member;
       } //$user_id = $_POST['kickMember']
-      
-      
+
+
       if ($templateVariables['load'] == 'manageMembers') {
         if ($_POST['rank']) {
           $member = $db->where('organization', $oclass->organization['id'])->where('id', $user_id)->join('org_ranks', 'rank_id = org_group', 'left outer')->where('owner_rank', 0)->getOne('users', 'username, id');
@@ -60,38 +60,38 @@
             $success = $member['username'] . '\'s rank updated';
           } //$member['id']
         } //$_POST['rank']
-        
-        
+
+
         $pages = new Paginator;
         $pages->items_total = $oclass->organization["nrm"];
         $pages->items_per_page = 15;
         $pages->paginate();
-      
+
         $members = $oclass->get_members($pages->limit);
-        
+
         $ranks = $db->where('org_id', $oclass->organization['id'])->where('owner_rank', 0)->get('org_ranks', null, 'name, rank_id');
-        
-        
+
+
         $templateVariables['ranks']   = $ranks;
         $templateVariables['members'] = $members;
       } //$templateVariables['load'] == 'manageMembers'
     } //$GET['view'] == 'manageMembers' && $oclass->perms['manageMembers']
     else if ($GET['view'] == 'manageRanks' && $oclass->perms['manageRanks']) {
       $templateVariables['load'] = 'manageRanks';
-      
+
       $oclass->editRanks();
-      
-      
+
+
     } //$GET['view'] == 'manageRanks' && $oclass->perms['manageRanks']
     else if ($GET['view'] == 'core' && $oclass->perms['manageOrganization']) {
       $templateVariables['load'] = 'edit_org';
-      
+
       $oclass->manageOrganization();
-      
+
     } //$GET['view'] == 'core' && $oclass->perms['manageOrganization']
     else if ($GET['view'] == 'changeOwner' && $oclass->perms['owner']) {
       $templateVariables['load'] = 'change_owner';
-      
+
       if ($_POST)
       {
         $member = $db->where('organization', $oclass->organization['id'])->where('id', $_POST['member'])
@@ -102,51 +102,42 @@
           $message = 'I, '.$user['username'].', have decided to surrender the position of organization owner to '.$member['username'].'.';
           $oclass->updateOrganization(array('owner' => $member['id']));
           $defaultRank = $db->where('org_id', $oclass->organization['id'])->where('default_rank', 1)->getOne('org_ranks', 'rank_id');
-          
+
           $uclass->updatePlayer(array('org_group' => $user['org_group']), $member['id']);
           $uclass->updatePlayer(array('org_group' => $defaultRank['rank_id']));
           $oclass->sendMessageToEveryone($message, $title);
           $cardinal->redirect($url);
-          
+
         }
       }
       $members = $oclass->get_members("", true);
-      
+
       $templateVariables['members'] = $members;
     } //$GET['view'] == 'core' && $oclass->perms['manageOrganization']
     else if ($GET['view'] == 'forum') {
       //$templateVariables['load']='forum';
-      
+
       $oclass->organizationForum();
-    
+
     } //$GET['view'] == 'forum'
     else if ($GET['view'] == 'disband' && $oclass->perms['owner']) {
       $templateVariables['load'] = 'disband';
-      
+
       if ($GET['do'] && $oclass->organization['nrm'] == 1)
         $oclass->disband_org();
-      
+
     } //$GET['view'] == 'disband' && $oclass->perms['owner']
     else if ($GET['view'] == 'applications' && $oclass->perms['manageApplications']) {
       $templateVariables['load'] = 'applications';
-      
+
       $oclass->manageApplications();
-      
+
     } //$GET['view'] == 'applications' && $oclass->perms['manageApplications']
-    else if ($GET['view'] == 'chat') {
-      $templateVariables['load'] = 'chat';
-      
-      if (!$_SESSION['org'])
-        $_SESSION['org'] = $user['organization'];
-      
-      $templateVariables['orgChat'] = true;
-      
-    } //$GET['view'] == 'chat'
     else if ($GET['view'] == 'leave' && !$oclass->perms['owner']) {
       $templateVariables['load'] = 'quit';
-      
+
       if ($_POST['quit']) {
-        
+
         $oclass->kickMember($user, true);
         $cardinal->redirect($url);
       } //$_POST['quit']
@@ -154,8 +145,8 @@
     elseif($GET['view'] == 'hackingPoints')
     {
     include ("org_hacking_points.php");
-    
+
     }
-    
-    
+
+
     $templateVariables['access'] = $oclass->perms;
